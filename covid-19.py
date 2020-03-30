97c053d1e2d813c1f5a7f47791d7dc9050f99aa2
Jens Haulund <jenshaulund@yahoo.com>
2020-03-29 21:10:45 -0400

    5 to 15

--- covid-19.py
+++ covid-19.py
@@ -26,7 +26,7 @@ print("Total number of rows: %d"%(csvreader.line_num))
 # printing the field names
 print('Field names are:' + ', '.join(field for field in fields))
 
-#  printing first 5 rows 
+#  printing first 15 rows 
 print('\nFirst 5 rows are:\n') 
 for row in rows[:15]: 
     print('Row contains:' + ', '.join(col for col in row))
